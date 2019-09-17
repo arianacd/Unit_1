b158d2a1a8e860f0c9532b076277a7225dd2f968
ariana <ariana.daney@ssfs.org>
2019-09-17 13:53:53 -0400

    made unit 1 project, option 1

--- unit1project_one.py
+++ unit1project_one.py
@@ -1,50 +1,43 @@
 # unit1project_one
 # by Ariana Daney
-# Last modified September 16, 2019
-#
-# This program makes four octagons of different colors
+# Last modified September 17, 2019
+# This program makes four octagons of different colors in different locations
 import turtle
 
-# function to make an octagon
+# This function is to make an octagon with a choice of color
 
 
-def makeanoctagon():
+def makeanoctagon(color):
+    turtle.color(color)
+    turtle.begin_fill()
     for x in range(8):
         turtle.fd(50)
         turtle.left(45)
+    turtle.end_fill()
 
-# function to change turtle's location
+# This function is to move the turtle to a choice location
 
 
-def go_to(x,y):
+def go_to(x, y):
     turtle.penup()
-    turtle.goto(x,y)
+    turtle.goto(x, y)
     turtle.down()
 
-# makes a hot pink octagon
+# These lines make a hot pink octagon
 
 
-turtle.color("hot pink")
-turtle.begin_fill()
-makeanoctagon()
-turtle.end_fill()
-# makes a turquoise octagon in a new location
-go_to(-150,0)
-turtle.color("turquoise")
-turtle.begin_fill()
-makeanoctagon()
-turtle.end_fill()
-# makes a magenta octagon in a new location
-go_to(-150,-150)
-turtle.color("magenta")
-turtle.begin_fill()
-makeanoctagon()
-turtle.end_fill()
-# makes a cyan octagon in a new location
-go_to(0,-150)
-turtle.color("cyan")
-turtle.begin_fill()
-makeanoctagon()
-turtle.end_fill()
-# the program stays on the screen until its clicked
+makeanoctagon("hot pink")
+
+# These lines make a turquoise octagon to the left of the previous
+go_to(-150, 0)
+makeanoctagon("turquoise")
+
+# These lines make a magenta octagon below the previous
+go_to(-150, -150)
+makeanoctagon("magenta")
+# These lines make a cyan octagon to the right of the previous
+go_to(0, -150)
+makeanoctagon("cyan")
+
+# This makes the program stay on the screen until its clicked
 turtle.exitonclick()
