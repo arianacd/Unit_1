9e546ef32fbe76040e3d506024762b4a3c1175ba
ariana <ariana.daney@ssfs.org>
2019-09-17 14:15:12 -0400

    made function to make houses and change color

--- shapes.py
+++ shapes.py
@@ -1,13 +1,17 @@
 import turtle
+
+# makes a triangle
 turtle.speed(0)
 turtle.forward(100)
 turtle.left(120)
 turtle.forward(100)
 turtle.left(120)
 turtle.forward(100)
+# turtle moves without being seen
 turtle.penup()
 turtle.forward(100)
 turtle.pendown()
+# turtle makes a square
 turtle.forward(100)
 turtle.left(90)
 turtle.forward(100)
@@ -15,20 +19,25 @@ turtle.left(90)
 turtle.forward(100)
 turtle.left(90)
 turtle.forward(100)
+# turtle moves without being seen
 turtle.penup()
 turtle.forward(100)
 turtle.pendown()
+# turtle makes a pentagon
 for x in range(5):
     turtle.forward(90)
     turtle.left(72)
+# turtle moves without being seen
 turtle.penup()
 turtle.forward(200)
 turtle.pendown()
+
 turtle.circle(50)
 turtle.right(90)
 turtle.penup()
 turtle.forward(100)
 turtle.pendown()
+# turtle makes a star
 for x in range(5):
     turtle.forward(50)
     turtle.left(144)
@@ -36,6 +45,7 @@ turtle.right(45)
 turtle.penup()
 turtle.forward(80)
 turtle.pendown()
+# turtle makes rotating squares
 for x in range(4):
     turtle.forward(80)
     turtle.left(90)
@@ -48,28 +58,39 @@ for x in range(4):
     turtle.forward(80)
     turtle.left(90)
 turtle.right(20)
+
 turtle.seth(0)
 turtle.penup()
-turtle.fd(100)
+turtle.goto(200,200)
 turtle.down()
+turtle.left(120)
 
-def make_a_house():
-    turtle.color("red")
+# function to make a house with different colors
+def make_a_house(color, color2):
+    turtle.color(color)
     turtle.begin_fill()
     for x in range(3):
         turtle.forward(80)
         turtle.left(120)
-    turtle.end_fill
-    turtle.lf(60)
-    turtle.color("blue")
+    turtle.end_fill()
+    turtle.left(60)
+    turtle.color(color2)
     turtle.begin_fill()
-    for x in range(4):
+    for y in range(4):
         turtle.fd(80)
         turtle.left(90)
-    turtle.end_color()
+    turtle.end_fill()
+
+def go_to(x, y):
+    turtle.penup()
+    turtle.goto(x, y)
+    turtle.down()
 
 
+make_a_house("red", "blue")
 
+go_to(-300, -300)
+make_a_house("green", "yellow")
 
 turtle.exitonclick()
 
